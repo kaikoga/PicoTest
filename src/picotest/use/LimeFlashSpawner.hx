68615e2ec4af257ba56d8225e9990f206cc222a7
kaikoga <info@mail.kaikoga.net>
2015-08-18 05:39:31 +0900

    LimeFlashSpawner will use flashLog()

--- src/picotest/use/LimeFlashSpawner.hx
+++ src/picotest/use/LimeFlashSpawner.hx
@@ -13,20 +13,7 @@ class LimeFlashSpawner extends TestSpawner {
 
 	override public function execute(reportFile:String):Void {
 		this.command('lime', ['run', 'flash']);
-
-		var flog:String = null;
-		if (Context.defined(PicoTestMacros.PICOTEST_FLOG)) flog = Context.definedValue(PicoTestMacros.PICOTEST_FLOG);
-		switch (systemName()) {
-			case "Windows":
-				if (flog == null) flog = '"%APPDATA%/Macromedia/Flash Player/Logs/flashlog.txt';
-			case "Linux":
-				if (flog == null) flog = '~/Macromedia/Flash_Player/Logs/flashlog.txt';
-			case "Mac":
-				if (flog == null) flog = '~/Library/Preferences/Macromedia/Flash\\ Player/Logs/flashlog.txt';
-			default:
-				throw 'Flash warning in platform ${systemName()} not supported';
-		}
-		this.command('cp', [flog, reportFile]);
+		this.command('cp', [flashLog(), reportFile]);
 	}
 
 	public static function toSpawn():LimeFlashSpawner {
