8ea39b49635100338708faf41e8c32bd3dab94a2
kaikoga <info@mail.kaikoga.net>
2015-10-25 19:20:01 +0900

    parametrize setup

--- src/picotest/readers/PicoTestReader.hx
+++ src/picotest/readers/PicoTestReader.hx
@@ -13,61 +13,73 @@ class PicoTestReader implements IPicoTestReader {
 		var className:String = Type.getClassName(testCaseClass);
 		var allMeta:Dynamic<Dynamic<Array<Dynamic>>> = haxe.rtti.Meta.getFields(testCaseClass);
 
+		var tests:Array<TestType> = [];
 		var hasSetup:Bool = false;
 		var hasTearDown:Bool = false;
+		var setupParameters:ParameterProvider = [[]];
+
 		for (field in Type.getInstanceFields(testCaseClass)) {
+			var meta:Dynamic<Array<Dynamic>>;
+			if (Reflect.hasField(allMeta, field)) {
+				meta = Reflect.field(allMeta, field);
+			} else {
+				meta = {};
+			}
+
+			var parameters:ParameterProvider = null;
+			if (Reflect.hasField(meta, "Parameter")) {
+				var metaParameter:Array<Dynamic> = Reflect.field(meta, "Parameter");
+				if (metaParameter == null) {
+					// default parameter
+					parameters = defaultParameters;
+				} else {
+					var parameterProviderName:String = Std.string(metaParameter[0]);
+					parameters = bind(createInstance(testCaseClass), parameterProviderName, [])();
+					if (parameters == null) parameters = defaultParameters;
+					if (parameters == null || !parameters.iterator().hasNext()) parameters = [[]];
+				}
+			} else {
+				parameters = [[]];
+			}
+
+			var testType:TestType;
 			switch (field) {
-				case "setup": hasSetup = true;
-				case "tearDown": hasTearDown = true;
+				case "setup":
+					hasSetup = true;
+					setupParameters = parameters;
+				case "tearDown":
+					hasTearDown = true;
+				default:
+					if (Reflect.hasField(meta, "Ignore")) {
+						var message:String = null;
+						var metaIgnore:Array<Dynamic> = Reflect.field(meta, "Ignore");
+						if (metaIgnore != null) {
+							message = metaIgnore.map(function(v:Dynamic):String return Std.string(v)).join("\n");
+						}
+						if (message == null) message = "";
+						tests.push(TestType.Ignore(field, message));
+					} else if (field.indexOf("test") == 0) {
+						tests.push(TestType.Test(field, parameters));
+					} else if (Reflect.hasField(meta, "Test")) {
+						tests.push(TestType.Test(field, parameters));
+					}
 			}
 		}
 
-		for (field in Type.getInstanceFields(testCaseClass)) {
-			var testType:TestType = TestType.None;
-			var parameterProviders:Array<String> = [];
-			if (field.indexOf("test") == 0) {
-				testType = TestType.Test;
-			}
-
-			if (Reflect.hasField(allMeta, field)) {
-				var meta:Dynamic<Array<Dynamic>> = Reflect.field(allMeta, field);
-				if (Reflect.hasField(meta, "Test")) {
-					testType = TestType.Test;
-				}
-				if (Reflect.hasField(meta, "Ignore")) {
-					var message:String = null;
-					var messages:Array<Dynamic> = Reflect.field(meta, "Ignore");
-					if (messages != null) {
-						message = messages.map(function(v:Dynamic):String return Std.string(v)).join("\n");
-					}
-					if (message == null) message = "";
-					testType = TestType.Ignore(message);
-				}
-				if (Reflect.hasField(meta, "Parameter")) {
-					var parameterProviderNames:Array<Dynamic> = Reflect.field(meta, "Parameter");
-					if (parameterProviderNames != null) {
-						parameterProviders = parameterProviderNames.map(function(v:Dynamic):String return Std.string(v));
-					}
-				}
-			}
-			switch (testType) {
-				case TestType.None:
-				case TestType.Test:
-					var parameters:Iterable<Array<Dynamic>> = null;
-					if (parameterProviders.length > 0) {
-						parameters = bind(createInstance(testCaseClass), parameterProviders[0], [])();
-					}
-					if (parameters == null) parameters = defaultParameters;
-					if (parameters == null || !parameters.iterator().hasNext()) parameters = [[]];
-					for (arguments in parameters) {
+		for (test in tests) {
+			switch (test) {
+				case TestType.Test(field, parameters):
+					for (setupArguments in setupParameters) {
+						for (arguments in parameters) {
 						var testCase:Dynamic = createInstance(testCaseClass);
 						var func:Void->Void = bind(testCase, field, arguments);
-						var setup:Void->Void = hasSetup ? bind(testCase, "setup", []) : null;
+						var setup:Void->Void = hasSetup ? bind(testCase, "setup", setupArguments) : null;
 						var tearDown:Void->Void = hasTearDown ? bind(testCase, "tearDown", []) : null;
 						var task = new PicoTestTestTask(new PicoTestResult(className, field, null, setup, tearDown), func);
 						runner.add(task);
 					}
-				case TestType.Ignore(message):
+					}
+				case TestType.Ignore(field, message):
 					var task = new PicoTestIgnoreTestTask(new PicoTestResult(className, field), message);
 					runner.add(task);
 			}
@@ -92,7 +104,8 @@ class PicoTestReader implements IPicoTestReader {
 }
 
 private enum TestType {
-	None;
-	Test;
-	Ignore(message:String);
+	Test(field:String, parameters:Iterable<Array<Dynamic>>);
+	Ignore(field:String, message:String);
 }
+
+private typedef ParameterProvider = Iterable<Array<Dynamic>>;
