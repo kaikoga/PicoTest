35635858bb700d12b7c6e04177e54ff024dd8f44
kaikoga <info@mail.kaikoga.net>
2018-05-30 18:16:35 +0900

    create directory before write

--- src/picotest/spawners/common/PicoTestExternalCommandHelper.hx
+++ src/picotest/spawners/common/PicoTestExternalCommandHelper.hx
@@ -1,11 +1,13 @@
 package picotest.spawners.common;
 
-import sys.io.File;
 import haxe.crypto.Base64;
 import haxe.io.Bytes;
+import haxe.io.Path;
 import haxe.macro.Context;
 import haxe.Serializer;
 import haxe.Unserializer;
+import sys.FileSystem;
+import sys.io.File;
 
 #if (sys || macro || macro_doc_gen)
 
@@ -25,11 +27,13 @@ class PicoTestExternalCommandHelper {
 	}
 
 	macro public static function anotherNekoArgs():Dynamic {
-		return macro PicoTestExternalCommandHelper.deserializeBase64($v{Context.definedValue(PicoTestExternalCommandHelper.PICOTEST_ANOTHER_NEKO_ARGS)});
+		var anotherNekoArgs:String = Context.definedValue(PicoTestExternalCommandHelper.PICOTEST_ANOTHER_NEKO_ARGS);
+		return macro PicoTestExternalCommandHelper.deserializeBase64($v{anotherNekoArgs});
 	}
 
 	public static function writeFile(data:Bytes, fileName:String = null):Void {
 		if (fileName != null) {
+			FileSystem.createDirectory(new Path(fileName).dir);
 			var out = File.write(fileName);
 			out.write(data);
 			out.close();
